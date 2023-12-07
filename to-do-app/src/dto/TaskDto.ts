5a4d4f37e870a9ed508bec236b26f1093919a894
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-12-07 18:03:24 +0530

    Implement save task

--- to-do-app/src/dto/TaskDto.ts
+++ to-do-app/src/dto/TaskDto.ts
@@ -4,8 +4,8 @@ export class TaskDto {
     // description:string;
     // status: boolean;
     // email: string;
-    constructor(public id: number, public description: string,
-                public status: boolean, public email: string) {
+    constructor(public id: number | null , public description: string,
+                public status: boolean | null, public email: string) {
         // this.id = id;
         // this.description = description;
         // this.status = status;
--- to-do-app/src/form/Form.tsx
+++ to-do-app/src/form/Form.tsx
@@ -2,28 +2,30 @@ import './Form.css';
 import React, {useRef, useState} from "react";
 import {useTaskDispatcher} from "../context/TaskContext.tsx";
 import {TaskDto} from "../dto/TaskDto.ts";
+import {saveTask} from "../service/task-service.tsx";
+import {useUser} from "../context/UserContext.tsx";
 
 export function Form() {
+    const user = useUser();
     const [value, setValue] = useState("");
     const txtRef = useRef<HTMLInputElement>(null);
     const taskDispatcher = useTaskDispatcher();
 
-    function handleSubmit(e: React.FormEvent){
+    function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
-        // Todo: Create a new task
-        taskDispatcher({
-            type: 'add',
-            task:
-                new TaskDto(1, value.trim(),
-                    false, '')
-        })
-        setValue("");
-        txtRef.current!.focus();
+        saveTask(new TaskDto(null, value, null, user?.email!))
+            .then(task => {
+                taskDispatcher({type: 'add', task});
+                setValue("");
+                txtRef.current!.focus();
+            }).catch(err => {
+            alert(err);
+        });
     }
 
     return (
         <form onSubmit={handleSubmit}
-            className="d-flex gap-2 p-2 border-bottom">
+              className="d-flex gap-2 p-2 border-bottom">
             <input className="form-control"
                    ref={txtRef}
                    value={value}
